ea34e09c6171f30ed4ce2a3f7795ef85ecc9d662
Imran Hasan <imran.h94@gmail.com>
2024-03-27 02:45:19 +0500

    finally outputted items on the screen

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 function ExpensesSummary({ periodName, expenses }) {
   const expensesSum = expenses.reduce((sum, expense) => {
-    return (sum = expense.amount);
+    return sum + expense.amount;
   }, 0);
 
   return (
