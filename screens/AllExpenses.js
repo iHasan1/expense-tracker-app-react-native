228edf577226a6440a8b109581446d206a2f4fed
Imran Hasan <imran.h94@gmail.com>
2024-03-27 01:51:55 +0500

    added all the dependencies required for stack and button navigation

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function AllExpenses() {
+  return (
+    <View>
+      <Text>AllExpenses Screen</Text>
+    </View>
+  );
+}
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function RecentExpenses() {
+  return (
+    <View>
+      <Text>RecentExpenses Screen</Text>
+    </View>
+  );
+}
+
+export default RecentExpenses;
