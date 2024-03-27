5c13480261768e63ea3fdc224450d3d33f6dfe7c
Imran Hasan <imran.h94@gmail.com>
2024-03-27 05:59:27 +0500

    added a custom button component and added it to expense management Screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -17,8 +18,24 @@ function ManageExpense({ route, navigation }) {
     console.log("delete");
   }
 
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    console.log("confirm");
+  }
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode={"flat"} onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -40,6 +57,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
