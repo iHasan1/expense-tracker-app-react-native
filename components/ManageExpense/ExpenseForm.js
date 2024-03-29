ef04b3a2fb8cea9dd107ff7dcbf5a60b4dc650d9
Imran Hasan <imran.h94@gmail.com>
2024-03-30 00:55:31 +0500

    moved add and cancel buttons from ManageExpense to ExpenseForm as well as related logic and styles

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 import Input from "./Input";
-import { useState } from "react";
+import Button from "../UI/Button";
 
-function ExpenseForm() {
+function ExpenseForm({ submitButtonLabel, onCancel, onSubmit }) {
   const [inputValues, setInputValues] = useState({
     amount: "",
     date: "",
@@ -18,6 +19,8 @@ function ExpenseForm() {
       };
     });
   }
+
+  function submitHandler() {}
 
   return (
     <View style={styles.form}>
@@ -51,6 +54,14 @@ function ExpenseForm() {
           onChangeText: inputChangedHandler.bind(this, "description"),
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode={"flat"} onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   );
 }
@@ -74,5 +85,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
